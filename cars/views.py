19b8e2712e7c18154ede10f2042d1af9b6ca7809
Daniel Reis <danielqreis@gmail.com>
2025-08-10 22:21:38 -0300

    feat(cars): implement search and sort for cars list
    Adds search functionality to filter cars by model.
    
    Retrieves the search term from the 'search' GET parameter.
    
    Filters the queryset using a case-insensitive 'contains' lookup.
    
    Sorts the car list by brand name by default.

--- cars/views.py
+++ cars/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from cars.models import Car
 
 
-# Create your views here.
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('brand')
+    search = request.GET.get('search')
+    if search:
+        cars = cars.filter(model__icontains=search)
+
+    
 
     return render(
         request,
