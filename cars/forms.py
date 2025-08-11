09a638419bd103fc444837821b532c62c011a1dd
Daniel Reis <danielqreis@gmail.com>
2025-08-11 13:32:37 -0300

    feat(cars): add validation for minimum car value
    Implements the clean_value method in CarModelForm.
    
    Adds custom validation for the 'value' field.
    
    Raises a validation error if the car value is less than 5000.
    
    Enforces a minimum value business rule for new car submissions.

--- cars/forms.py
+++ cars/forms.py
@@ -5,3 +5,10 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 5000:
+            self.add_error('value', 'The car value must be higher than $5,000')
+        return value
