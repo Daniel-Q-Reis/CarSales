3d22563c3bc8452faae8ce47446572a87be0605e
Daniel Reis <danielqreis@gmail.com>
2025-08-14 04:52:09 -0300

    feat(cars): implement full CRUD functionality for cars
    Implements CarUpdateView and CarDeleteView for editing and deleting cars.
    
    Secures the create, update, and delete views, requiring user authentication using @method_decorator.
    
    Creates the corresponding templates for the new views: car_update.html and car_delete.html.
    
    Adds the final URL patterns for all car CRUD operations (detail, update, delete) in urls.py.
    
    Updates car_detail.html to correctly link to the new edit and delete pages.

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from cars.models import Car
 from cars.forms import CarModelForm
 
@@ -16,8 +19,32 @@ class CarsListView(ListView):
         return cars         
 
 
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    
+    def get_success_url(self):
+        return reverse('car_detail', kwargs={'pk': self.object.pk})
+    
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
+    
